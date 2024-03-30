588b1ee5068fd83d4e8e95bf726440f07797ddee
Szkárosi Szilárd (DLWGQZ) <90700865+szkarosisz1@users.noreply.github.com>
2024-03-30 20:24:17 +0100

    Updated Component and images

--- library/src/app/home/home.component.ts
+++ library/src/app/home/home.component.ts
@@ -1,12 +1,39 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    FormsModule,
+    MatIconModule,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrl: './home.component.css',
 })
+
 export class HomeComponent {
-  
+  selectedImage = 1;
+  slideAnimation = false;
+
+  nextImage() {
+    this.selectedImage++; 
+    if (this.selectedImage > 3) { 
+      this.selectedImage = 1; 
+    }
+    this.slideAnimation = true;
+    setTimeout(() => this.slideAnimation = false, 300); 
+  }
+
+  prevImage() {
+    this.selectedImage--; 
+    if (this.selectedImage < 1) { 
+      this.selectedImage = 3;
+    }
+    this.slideAnimation = true;
+    setTimeout(() => this.slideAnimation = false, 300); 
+  }
 }
