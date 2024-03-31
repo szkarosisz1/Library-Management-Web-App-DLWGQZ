4720dba05f1c1356409cf52c609e61c835ba5fe7
Szkárosi Szilárd (DLWGQZ) <90700865+szkarosisz1@users.noreply.github.com>
2024-03-31 23:04:21 +0200

    BookListComponent done

--- library/src/app/book-list/book-list.component.ts
+++ library/src/app/book-list/book-list.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { BookDTO } from '../../../model/library.dto';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatCardModule  
+  ],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.css'
 })
 export class BookListComponent {
-
+  books : BookDTO[] = [];
 }
