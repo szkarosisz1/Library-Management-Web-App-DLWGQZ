0a810ea44666580cf4641bed5eca9acec9996012
Szkárosi Szilárd (DLWGQZ) <90700865+szkarosisz1@users.noreply.github.com>
2024-03-31 23:49:20 +0200

    BookListComponent fixed

--- library/src/app/book-list/book-list.component.ts
+++ library/src/app/book-list/book-list.component.ts
@@ -1,18 +1,40 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BookDTO } from '../../../model/library.dto';
-import {MatCardModule} from '@angular/material/card';
+import { MatTableModule } from "@angular/material/table";
+import { BookService } from '../service/book.service';
+
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
   imports: [
     CommonModule,
-    MatCardModule  
+    MatTableModule
   ],
   templateUrl: './book-list.component.html',
   styleUrl: './book-list.component.css'
 })
-export class BookListComponent {
-  books : BookDTO[] = [];
+
+export class BookListComponent implements OnInit {
+  books: BookDTO[] = [];
+  displayedColumns: string[] = ['title', 'author', 'acquisitionDate', 'serialNumber', 'status'];
+
+  constructor(private bookService: BookService) { }
+
+  ngOnInit(): void {
+    this.loadBooks();
+  }
+
+  loadBooks(): void {
+    this.bookService.getAll().subscribe({
+      next: (books) => {
+        this.books = books;
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
+  
 }
