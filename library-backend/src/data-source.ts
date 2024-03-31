97b695fed631770539080c3b00af45f39b6e178b
Szkárosi Szilárd (DLWGQZ) <90700865+szkarosisz1@users.noreply.github.com>
2024-03-31 08:00:00 +0200

    data-source.ts modified

--- library-backend/src/data-source.ts
+++ library-backend/src/data-source.ts
@@ -1,17 +1,21 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "./entity/User"
+import { Book } from "./entity/Book"
+import { Borrow } from "./entity/Borrow"
+import { Cassette } from "./entity/Cassette"
+import { DVD } from "./entity/DVD"
+import { Member } from "./entity/Member"
 
 export const AppDataSource = new DataSource({
     type: "mysql",
     host: "localhost",
     port: 3306,
     username: "root",
-    password: "root",
-    database: "test",
+    database: "library",
     synchronize: true,
     logging: false,
-    entities: [User],
+    entities: [User, Book, Borrow, Cassette, DVD, Member],
     migrations: [],
     subscribers: [],
 })
