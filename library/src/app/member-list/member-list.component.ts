450179a1a393946fedd36505d5e4c0c4b41d4ded
Szkárosi Szilárd (DLWGQZ) <90700865+szkarosisz1@users.noreply.github.com>
2024-04-01 13:08:49 +0200

    Fixed Components

--- library/src/app/member-list/member-list.component.ts
+++ library/src/app/member-list/member-list.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { MemberDTO } from '../../../model/library.dto';
+import { MemberService } from '../service/member.service';
 
 @Component({
   selector: 'app-member-list',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatTableModule,
+    HttpClientModule
+  ],
   templateUrl: './member-list.component.html',
   styleUrl: './member-list.component.css'
 })
 export class MemberListComponent {
+  members: MemberDTO[] = [];
+  displayedColumns: string[] = ['name', 'phoneNumber', 'identityNumber', 'address', 'status'];
+
+  constructor(private memberService: MemberService) { }
+
+  ngOnInit(): void {
+    this.loadMembers();
+  }
+
+  loadMembers(): void {
+    this.memberService.getAll().subscribe({
+      next: (members) => {
+        this.members = members;
+      },
+      error: (err) => {
+        console.log(err);
+      }
+    })
+  }
 
 }
